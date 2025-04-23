bf1a1c0eced872bd234d53c6f1a45397fc3f35c2
Valeria Hernández <105462923+valerih03@users.noreply.github.com>
2025-04-23 11:19:22 -0600

    cambios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,10 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { ConsultarComponent } from './consultar/consultar.component';
 import { ActualizarComponent } from './actualizar/actualizar.component';
 import { DespacharComponent } from './despachar/despachar.component';
-import { DetdespachoComponent } from './detdespacho/detdespacho.component';
 export const routes: Routes = [
   {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
   {path: 'dashboard', component: DashboardComponent},
   {path: 'consultar', component: ConsultarComponent},
   {path: 'actualizar', component: ActualizarComponent},
   {path: 'despachar', component: DespacharComponent},
-  {path: 'detalledespacho', component: DetdespachoComponent},
 ];
