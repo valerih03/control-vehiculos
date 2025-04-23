70db5a420eea3ffb2574002b018a851ca020a40f
Vane Rosales <jvanessa.rr@gmail.com>
2025-04-23 11:29:46 -0600

    Rutas con lazy-load(loadComponent)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,9 +4,21 @@ import { ConsultarComponent } from './consultar/consultar.component';
 import { ActualizarComponent } from './actualizar/actualizar.component';
 import { DespacharComponent } from './despachar/despachar.component';
 export const routes: Routes = [
-  {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
-  {path: 'dashboard', component: DashboardComponent},
-  {path: 'consultar', component: ConsultarComponent},
-  {path: 'actualizar', component: ActualizarComponent},
-  {path: 'despachar', component: DespacharComponent},
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./dashboard/dashboard.component').then(m => m.DashboardComponent)
+  },
+  {
+    path: 'consultar',
+    loadComponent: () => import('./consultar/consultar.component').then(m => m.ConsultarComponent)
+  },
+  {
+    path: 'actualizar',
+    loadComponent: () => import('./actualizar/actualizar.component').then(m => m.ActualizarComponent)
+  },
+  {
+    path: 'despachar',
+    loadComponent: () => import('./despachar/despachar.component').then(m => m.DespacharComponent)
+  },
 ];
