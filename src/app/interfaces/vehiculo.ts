1537a2bdd661a6188044a9dee4993889eda2a91d
Vane Rosales <jvanessa.rr@gmail.com>
2025-04-30 15:29:19 -0600

    No funciona, pero actualice la interfaz de vehiculos

--- src/app/interfaces/vehiculo.ts
+++ src/app/interfaces/vehiculo.ts
@@ -1,10 +1,15 @@
 export interface Vehiculo {
+  id: number;
+  fechaIngreso: Date;
+  tarja: string;
+  numeroBL: string;
+  consignatario: string;
   vin: string;
+  nit: string;
   marca: string;
   estilo: string;
   color: string;
   anio: number;
   estado: string;
   observaciones: string;
-  tarja: string; // Relación con Ingreso
 }
