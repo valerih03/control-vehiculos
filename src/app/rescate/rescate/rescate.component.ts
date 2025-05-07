41e8acbbcb06bc0ca6822a8748b86e2492e4fab0
Vane Rosales <jvanessa.rr@gmail.com>
2025-05-07 13:02:20 -0600

    Agregando la parte de rescate

---
+++ src/app/rescate/rescate/rescate.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rescate',
+  standalone: true,
+  imports: [],
+  templateUrl: './rescate.component.html',
+  styleUrl: './rescate.component.css'
+})
+export class RescateComponent {
+
+}
